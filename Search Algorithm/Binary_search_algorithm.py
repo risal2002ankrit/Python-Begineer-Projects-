c07db5e1c450a3ce6063655eb05ef74a647ba064
Ankrit <risalankrit2002@gmail.com>
2023-06-23 21:02:51 +0545

    program completed

--- Search Algorithm/Binary_search_algorithm.py
+++ Search Algorithm/Binary_search_algorithm.py
@@ -7,10 +7,26 @@
 def binary_search(list, elements):
     middle = 0
     start = 0
-    end =len(list)
+    end = len(list)
     steps = 0
 
-    while(start<=end):
-        print("Steps",steps, ":" ,str(list[start:end+1]))
+    while(start <= end):
+        print("Steps", steps, ":", str(list[start:end+1]))
+
         steps = steps+1
-        middle= (start+end)/2
+        middle= int((start+end)/2)
+
+        if elements == list[middle]:
+            return middle
+        if elements < list[middle]:
+            end = middle-1
+        else:
+            start = middle + 1
+
+    return -1
+
+my_list = [1,2,3,4,5,6,7,8,9]
+target = 6
+
+binary_search(my_list, target)
+
